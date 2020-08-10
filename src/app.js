c4c7c5aa37942cd5e35684d0a843822cbb822687
Pedro Kautzmann <pedrokautzmann1@gmail.com>
2020-08-10 11:08:22 -0300

    feature: all tests passed

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,75 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  let repository = repositories.find(repo => id === repo.id);
+
+  if (!repository) {
+    return response.status(400).json({ error: 'This repository does not exists' });
+  }
+
+  repository = {
+    ...repository,
+    title,
+    url,
+    techs
+  }
+
+  repositories.push([...repositories, repository]);
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repo => id === repo.id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'This repository does not exists' });
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repo => id === repo.id);
+
+  const repository = repositories.find(repo => id === repo.id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'This repository does not exists' });
+  }
+
+  repositories[repositoryIndex] = { ...repository, likes: repository.likes + 1 };
+
+  return response.json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
